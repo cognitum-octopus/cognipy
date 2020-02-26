40d72aca85b5954fefd46e3f1116a9e73e1ae586
pkaplanski <p.kaplanski@cognitum.eu>
2020-02-26 21:32:07 +1100

    cleanup

--- cognipy/CogniPyLib/buildNuGetPackage.bat
+++
@@ -1,4 +0,0 @@
-cd %~dp0
-REM nuget spec -f
-nuget pack FluentEditorClientLib.csproj -build -IncludeReferencedProjects -Prop Configuration=Release
-REM COPY *.nupkg "\\cogserver01\PUBLIC\NuGet packages library"
