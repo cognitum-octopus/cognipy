d63c002070f9c557ea2a132df609c2fc2551afdd
kaplan196883 <pawel.kaplanski@cognitum.eu>
2020-10-31 15:50:19 +1100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cognipy',
-    version='0.1.7',
+    version='0.1.8',
     author="Cognitum Services S.A.",
     author_email="support@cognitum.eu",
     description="Semantic Tech Reasoner and Editor",
