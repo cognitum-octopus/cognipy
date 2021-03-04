a7060b78c9396ca9a4f38c321e9c93748c6ee40e
unknown <pawel.kaplanski@cognitum.eu>
2021-03-04 18:10:14 +1100

    broken repo

---
+++ cognipy/CogniPyCLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CogniPyCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InteractiveMode.EntryPoint(args);
+        }
+    }
+}
